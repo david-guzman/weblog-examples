8d4343a8e7b464c8908d71f9465dbf8e506f2e15
David Guzman <d.guzman@ucl.ac.uk>
2017-01-30 20:15:30 +0000

    Added web module

---
+++ jaspic-basic/jaspic-basic-web/src/main/java/guzman/weblog/jaspic/basic/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package guzman.weblog.jaspic.basic.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+}
