3a8154e4d1562bf224daa54f9767ec7e0f8a9bc2
David Guzman <d.guzman@ucl.ac.uk>
2017-01-30 19:01:00 +0000

    Added unit tests

--- jaspic-basic/jaspic-basic-sam/src/main/java/guzman/weblog/jaspic/basic/HeaderBasicAuthModule.java
+++ jaspic-basic/jaspic-basic-sam/src/main/java/guzman/weblog/jaspic/basic/HeaderBasicAuthModule.java
@@ -1,13 +1,19 @@
 package guzman.weblog.jaspic.basic;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Map;
 import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
 import javax.security.auth.message.AuthException;
 import javax.security.auth.message.AuthStatus;
 import javax.security.auth.message.MessageInfo;
 import javax.security.auth.message.MessagePolicy;
+import javax.security.auth.message.callback.PasswordValidationCallback;
 import javax.security.auth.message.module.ServerAuthModule;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,9 +28,32 @@ public class HeaderBasicAuthModule implements ServerAuthModule {
     HttpServletRequest.class, HttpServletResponse.class
   };
 
+  private MessagePolicy requestPolicy;
+  private CallbackHandler handler;
+  private Map options;
+  // Not secure connections must be allowed explicitly
+  private boolean allowNotSecure = false;
+  private static final String ALLOW_NOTSECURE_KEY = "allow.notsecure";
+  private static final String BASIC = "Basic";
+  private static final String AUTHORISATION_HEADER = "Authorization";
+  private static final String DEF_NOT_SEC = "false";
+  private static final String DEF_ENC = "UTF-8";
+  private PasswordValidationCallback passwordValidationCallback;
+
   @Override
-  public void initialize(MessagePolicy mp, MessagePolicy mp1, CallbackHandler ch, Map map) throws AuthException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public void initialize(
+          MessagePolicy requestPolicy, 
+          MessagePolicy responsePolicy, 
+          CallbackHandler handler, 
+          Map options) throws AuthException {
+    this.requestPolicy = requestPolicy;
+    this.handler = handler;
+    this.options = options;
+    if (null != this.options) {
+      allowNotSecure = Boolean.parseBoolean(
+                (String) options.getOrDefault(ALLOW_NOTSECURE_KEY, DEF_NOT_SEC)
+      );
+    }
   }
 
   @Override
@@ -33,18 +62,93 @@ public class HeaderBasicAuthModule implements ServerAuthModule {
   }
 
   @Override
-  public AuthStatus validateRequest(MessageInfo mi, Subject sbjct, Subject sbjct1) throws AuthException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public AuthStatus validateRequest(
+          MessageInfo messageInfo, 
+          Subject clientSubject, 
+          Subject serviceSubject
+  ) throws AuthException {
+    final HttpServletRequest req = (HttpServletRequest) messageInfo.getRequestMessage();
+    
+    // Evaluate whether not secure connections are allowed against policy
+    if (!req.isSecure() && !allowNotSecure) {
+      return AuthStatus.SEND_FAILURE;
+    }
+    
+    if (!requestPolicy.isMandatory()) {
+
+      final String userName = readAuthenticationHeader(messageInfo, clientSubject);
+
+      if (null == userName) {
+        return AuthStatus.FAILURE;
+      }
+
+      return AuthStatus.SUCCESS;
+      
+    } else {
+      return AuthStatus.SUCCESS;
+    }
   }
 
   @Override
   public AuthStatus secureResponse(MessageInfo mi, Subject sbjct) throws AuthException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    return AuthStatus.SEND_SUCCESS;
   }
 
   @Override
-  public void cleanSubject(MessageInfo mi, Subject sbjct) throws AuthException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public void cleanSubject(MessageInfo mi, Subject subject) throws AuthException {
+    if (null != subject) {
+      subject.getPrincipals().clear();
+    }
   }
   
+  private String readAuthenticationHeader(MessageInfo msgInfo, Subject subj) throws AuthException {
+
+    HttpServletRequest request = (HttpServletRequest) msgInfo.getRequestMessage();
+    String header = request.getHeader(AUTHORISATION_HEADER);
+    
+    if (!isEmpty(header) && header.startsWith(BASIC + " ")) {
+
+      header = header.substring(6).trim();
+      
+      // Decode and parse the authorization header
+      byte[] headerBytes = header.getBytes(Charset.forName(DEF_ENC));
+      byte[] decBytes = Base64.getDecoder().decode(headerBytes);
+      String decoded = new String(decBytes, Charset.forName(DEF_ENC));
+
+      int colon = decoded.indexOf(':');
+      if (colon <= 0 || colon == decoded.length() - 1) {
+        return (null);
+      }
+
+      String username = decoded.substring(0, colon);
+      
+      // use the callback to ask the container to
+      // validate the password      
+      if (null == passwordValidationCallback) {
+        passwordValidationCallback = new PasswordValidationCallback(
+              subj, username, decoded.substring(colon + 1).toCharArray());
+      }
+      try {
+        handler.handle(new Callback[]{passwordValidationCallback});
+        passwordValidationCallback.clearPassword();
+      } catch (IOException | UnsupportedCallbackException ex) {
+        AuthException ae = new AuthException();
+        ae.initCause(ex);
+        throw ae;
+      }
+
+      if (passwordValidationCallback.getResult()) {
+        return username;
+      }
+    }
+    return null;
+  }
+  
+  protected void setPasswordValidationCallback(PasswordValidationCallback pwdValidationCallback) {
+    this.passwordValidationCallback = pwdValidationCallback;
+  }
+  
+  private boolean isEmpty(String text) {
+    return text == null || text.isEmpty();
+  }
 }
---
+++ jaspic-basic/jaspic-basic-sam/src/test/java/guzman/weblog/jaspic/basic/HeaderBasicAuthModuleNGTest.java
@@ -0,0 +1,137 @@
+package guzman.weblog.jaspic.basic;
+
+import java.security.Principal;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.message.AuthException;
+import javax.security.auth.message.AuthStatus;
+import javax.security.auth.message.MessageInfo;
+import javax.security.auth.message.MessagePolicy;
+import javax.security.auth.message.callback.PasswordValidationCallback;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+
+/**
+ *
+ * @author David Guzman {@literal d.guzman at ucl.ac.uk}
+ */
+public class HeaderBasicAuthModuleNGTest {
+  
+  private final Map<String, String> options;
+  private final HeaderBasicAuthModule module = new HeaderBasicAuthModule();
+  private final CallbackHandler handler = mock(CallbackHandler.class);
+  private final MessagePolicy mockRequestPolicy = mock(MessagePolicy.class);
+  
+  public HeaderBasicAuthModuleNGTest() {
+    options = Collections.unmodifiableMap(new HashMap());
+    when(mockRequestPolicy.isMandatory()).thenReturn(false);
+  }
+
+  @BeforeClass
+  public static void setUpClass() throws Exception {
+  }
+
+  @AfterClass
+  public static void tearDownClass() throws Exception {
+  }
+
+  @BeforeMethod
+  public void setUpMethod() throws Exception {
+    module.initialize(mockRequestPolicy, null, handler, options);
+  }
+
+  @AfterMethod
+  public void tearDownMethod() throws Exception {
+  }
+
+  /**
+   * Test of getSupportedMessageTypes method, of class HeaderBasicAuthModule.
+   */
+  @Test
+  public void testGetSupportedMessageTypes() {
+    System.out.println("getSupportedMessageTypes");
+    Class<?>[] messageTypes = new Class<?>[]{HttpServletRequest.class, HttpServletResponse.class};
+    assertEquals(module.getSupportedMessageTypes(), messageTypes);
+  }
+
+  @Test
+  public void testValidateRequestSecure() throws AuthException {
+    final MessageInfo messageInfo = mock(MessageInfo.class);
+    final HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+    when(messageInfo.getRequestMessage()).thenReturn(servletRequest);
+    final Subject client = new Subject();
+    
+    // Connections that are not secure must return SEND_FAILURE by default
+    assertEquals(module.validateRequest(messageInfo, client, null), AuthStatus.SEND_FAILURE);
+  }
+  
+  @Test
+  public void testValidateRequestAuthHeader() throws AuthException {
+    final MessageInfo messageInfo = mock(MessageInfo.class);
+    final HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+    when(messageInfo.getRequestMessage()).thenReturn(servletRequest);
+    final Subject client = new Subject();
+    // Return TRUE for isSecure()
+    when(servletRequest.isSecure()).thenReturn(Boolean.TRUE);
+    // Token authorisation mandatory for all resources except for authentication
+    when(mockRequestPolicy.isMandatory()).thenReturn(Boolean.FALSE);
+    // HttpServletRequest header returns null;
+    assertEquals(module.validateRequest(messageInfo, client, null), AuthStatus.FAILURE);
+  }
+  
+  @Test
+  public void testValidateRequestAuthBasic() throws AuthException {
+    final MessageInfo messageInfo = mock(MessageInfo.class);
+    final HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+    when(messageInfo.getRequestMessage()).thenReturn(servletRequest);
+    final Subject client = new Subject();
+    // Return TRUE for isSecure()
+    when(servletRequest.isSecure()).thenReturn(Boolean.TRUE);
+    // Token authorisation mandatory for all resources except for authentication
+    when(mockRequestPolicy.isMandatory()).thenReturn(Boolean.FALSE);
+    
+    when(servletRequest.getHeader("Authorization")).thenReturn("Basic dXNlcjpwYXNzd29yZA==");
+    PasswordValidationCallback pwc = mock(PasswordValidationCallback.class);
+    when(pwc.getResult()).thenReturn(Boolean.TRUE);
+    module.setPasswordValidationCallback(pwc);
+    assertEquals(module.validateRequest(messageInfo, client, null), AuthStatus.SUCCESS);
+  }
+
+  /**
+   * Test of secureResponse method, of class HeaderBasicAuthModule.
+   */
+  @Test
+  public void testSecureResponse() throws Exception {
+    System.out.println("secureResponse");
+    final MessageInfo messageInfo = mock(MessageInfo.class);
+    final Subject subject = new Subject();
+    assertEquals(module.secureResponse(messageInfo, subject), AuthStatus.SEND_SUCCESS);
+  }
+
+  /**
+   * Test of cleanSubject method, of class HeaderBasicAuthModule.
+   */
+  @Test
+  public void testCleanSubject() throws Exception {
+    System.out.println("cleanSubject");
+    Subject subject = new Subject();
+    Principal principal = () -> "testPrincipal";
+    subject.getPrincipals().add(principal);
+    module.cleanSubject(null, subject);
+    assertEquals(subject.getPrincipals().size(), 0);
+  }
+  
+}
